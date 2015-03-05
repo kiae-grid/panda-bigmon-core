447f056c5b4bccb9fd47d63bd1a6c95891e123de
maria-grigorieva <magsend@gmail.com>
2015-03-05 13:42:14 +0300

    cassandra model modified

--- core/pandajob/cassandra_models.py
+++ core/pandajob/cassandra_models.py
@@ -109,7 +109,56 @@ class jobs(Model):
     workqueue_id = columns.Integer()
 
 
-class day_site_errors_30m(Model):
+# class day_site_errors_30m(Model):
+#     __compaction__ = SizeTieredCompactionStrategy
+#     __compaction_max_compaction_threshold__ = 32
+#     __compaction_min_compaction_threshold__ = 4
+#     __read_repair_chance__ = 0.0
+#     __dclocal_read_repair_chance__ = 0.1
+#     __default_time_to_live__ = 0
+#     __gc_grace_seconds__ = 864000
+#     __memtable_flush_period_in_ms__ = 0
+#     date = columns.DateTime(primary_key=True)
+#     computingsite = columns.Text(primary_key=True)
+#     base_mtime = columns.DateTime(primary_key=True)
+#     errcode = columns.Text(primary_key=True)
+#     diag = columns.Text(primary_key=True)
+#     count = columns.Counter()
+# 
+# class day_index(Model):
+#     __compaction__ = SizeTieredCompactionStrategy
+#     __compaction_max_compaction_threshold__ = 32
+#     __compaction_min_compaction_threshold__ = 4
+#     __read_repair_chance__ = 0.0
+#     __dclocal_read_repair_chance__ = 0.1
+#     __default_time_to_live__ = 0
+#     __gc_grace_seconds__ = 864000
+#     __memtable_flush_period_in_ms__ = 0
+#     date = columns.DateTime(primary_key=True)
+#     modificationtime = columns.DateTime(primary_key=True)
+#     pandaid = columns.Integer(primary_key=True)
+
+# CREATE TABLE bigpanda_archive.day_errors_30m (
+#     date timestamp,
+#     base_mtime timestamp,
+#     "count" counter,
+#     PRIMARY KEY (date, base_mtime)
+# ) WITH CLUSTERING ORDER BY (base_mtime ASC)
+#     AND bloom_filter_fp_chance = 0.01
+#     AND caching = '{"keys":"ALL", "rows_per_partition":"NONE"}'
+#     AND comment = ''
+#     AND compaction = {'min_threshold': '4', 'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32'}
+#     AND compression = {'sstable_compression': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+#     AND dclocal_read_repair_chance = 0.1
+#     AND default_time_to_live = 0
+#     AND gc_grace_seconds = 864000
+#     AND max_index_interval = 2048
+#     AND memtable_flush_period_in_ms = 0
+#     AND min_index_interval = 128
+#     AND read_repair_chance = 0.0
+#     AND speculative_retry = '99.0PERCENTILE';
+
+class day_errors_30m(Model):
     __compaction__ = SizeTieredCompactionStrategy
     __compaction_max_compaction_threshold__ = 32
     __compaction_min_compaction_threshold__ = 4
@@ -118,14 +167,33 @@ class day_site_errors_30m(Model):
     __default_time_to_live__ = 0
     __gc_grace_seconds__ = 864000
     __memtable_flush_period_in_ms__ = 0
-    date = columns.DateTime(primary_key=True)
-    computingsite = columns.Text(primary_key=True)
-    base_mtime = columns.DateTime(primary_key=True)
-    errcode = columns.Text(primary_key=True)
-    diag = columns.Text(primary_key=True)
+    date = columns.DateTime(primary_key = True)
+    base_mtime = columns.DateTime(primary_key = True)
     count = columns.Counter()
 
-class day_index(Model):
+# CREATE TABLE bigpanda_archive.day_site_errors (
+#     date timestamp,
+#     computingsite text,
+#     errcode text,
+#     pandaid int,
+#     diag text,
+#     PRIMARY KEY (date, computingsite, errcode, pandaid)
+# ) WITH CLUSTERING ORDER BY (computingsite ASC, errcode ASC, pandaid ASC)
+#     AND bloom_filter_fp_chance = 0.01
+#     AND caching = '{"keys":"ALL", "rows_per_partition":"NONE"}'
+#     AND comment = ''
+#     AND compaction = {'min_threshold': '4', 'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32'}
+#     AND compression = {'sstable_compression': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+#     AND dclocal_read_repair_chance = 0.1
+#     AND default_time_to_live = 0
+#     AND gc_grace_seconds = 864000
+#     AND max_index_interval = 2048
+#     AND memtable_flush_period_in_ms = 0
+#     AND min_index_interval = 128
+#     AND read_repair_chance = 0.0
+#     AND speculative_retry = '99.0PERCENTILE';
+
+class day_site_errors(Model):
     __compaction__ = SizeTieredCompactionStrategy
     __compaction_max_compaction_threshold__ = 32
     __compaction_min_compaction_threshold__ = 4
@@ -134,7 +202,49 @@ class day_index(Model):
     __default_time_to_live__ = 0
     __gc_grace_seconds__ = 864000
     __memtable_flush_period_in_ms__ = 0
-    date = columns.DateTime(primary_key=True)
-    modificationtime = columns.DateTime(primary_key=True)
-    pandaid = columns.Integer(primary_key=True)
+    date = columns.DateTime(primary_key = True)
+    computingsite = columns.Text(primary_key = True)
+    errcode = columns.Integer(primary_key = True)
+    pandaid = columns.Integer(primary_key = True)
+    diag = columns.Text()
     
+# CREATE TABLE bigpanda_archive.day_site_errors_cnt_30m (
+#     date timestamp,
+#     computingsite text,
+#     base_mtime timestamp,
+#     errcode text,
+#     diag text,
+#     err_count counter,
+#     job_count counter,
+#     PRIMARY KEY (date, computingsite, base_mtime, errcode, diag)
+# ) WITH CLUSTERING ORDER BY (computingsite ASC, base_mtime ASC, errcode ASC, diag ASC)
+#     AND bloom_filter_fp_chance = 0.01
+#     AND caching = '{"keys":"ALL", "rows_per_partition":"NONE"}'
+#     AND comment = ''
+#     AND compaction = {'min_threshold': '4', 'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32'}
+#     AND compression = {'sstable_compression': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+#     AND dclocal_read_repair_chance = 0.1
+#     AND default_time_to_live = 0
+#     AND gc_grace_seconds = 864000
+#     AND max_index_interval = 2048
+#     AND memtable_flush_period_in_ms = 0
+#     AND min_index_interval = 128
+#     AND read_repair_chance = 0.0
+#     AND speculative_retry = '99.0PERCENTILE';
+
+class day_site_errors_cnt_30m(Model):
+    __compaction__ = SizeTieredCompactionStrategy
+    __compaction_max_compaction_threshold__ = 32
+    __compaction_min_compaction_threshold__ = 4
+    __read_repair_chance__ = 0.0
+    __dclocal_read_repair_chance__ = 0.1
+    __default_time_to_live__ = 0
+    __gc_grace_seconds__ = 864000
+    __memtable_flush_period_in_ms__ = 0
+    date = columns.DateTime(primary_key = True)
+    computingsite = columns.Text(primary_key = True)
+    base_mtime = columns.DateTime(primary_key = True)
+    errcode = columns.Integer(primary_key = True)
+    diag = columns.Text(primary_key = True)
+    err_count = columns.Counter()
+    job_count = columns.Counter()
