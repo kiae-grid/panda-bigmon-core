0f618635283dbfcf23f7cf6bdbeecc1e0265db05
Eygene Ryabinkin <rea@grid.kiae.ru>
2015-04-03 18:01:56 +0300

    Add model for table day_mtime_site_errors_cnt_30m
    It is a variant of day_site_errors_cnt_30m with rearranged primary key
    columns to allow queries like
    {{{
    select <BLAH> from day_mtime_site_errors_cnt_30m
      where date in ('2014-01-01','2014-01-02','2014-01-03') and
      base_mtime > '2014-01-01 12:00' and base_mtime < '2014-01-03 05:00:00';
    }}}
    
    Signed-off-by: Eygene Ryabinkin <rea@grid.kiae.ru>

--- core/pandajob/cassandra_models.py
+++ core/pandajob/cassandra_models.py
@@ -355,3 +355,51 @@ class day_site_errors_cnt_30m(Model):
     __gc_grace_seconds__ = 864000
     __memtable_flush_period_in_ms__ = 0
     __read_repair_chance__ = 0.0
+
+
+"""
+CQL statements for this table
+=============================
+
+CREATE TABLE panda_archive.day_mtime_site_errors_cnt_30m (
+    date timestamp,
+    base_mtime timestamp,
+    computingsite text,
+    errcode text,
+    diag text,
+    err_count int,
+    job_count int,
+    PRIMARY KEY (date, base_mtime, computingsite, errcode, diag)
+) WITH CLUSTERING ORDER BY (base_mtime ASC, computingsite ASC, errcode ASC, diag ASC)
+    AND bloom_filter_fp_chance = 0.01
+    AND caching = '{"keys":"ALL", "rows_per_partition":"NONE"}'
+    AND comment = ''
+    AND compaction = {'min_threshold': '4', 'class': 'org.apache.cassandra.db.compaction.SizeTieredCompactionStrategy', 'max_threshold': '32'}
+    AND compression = {'sstable_compression': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+    AND dclocal_read_repair_chance = 0.1
+    AND default_time_to_live = 0
+    AND gc_grace_seconds = 864000
+    AND max_index_interval = 2048
+    AND memtable_flush_period_in_ms = 0
+    AND min_index_interval = 128
+    AND read_repair_chance = 0.0
+    AND speculative_retry = '99.0PERCENTILE';
+"""
+
+class day_mtime_site_errors_cnt_30m(Model):
+    date = columns.DateTime(primary_key = True)
+    base_mtime = columns.DateTime(primary_key = True)
+    computingsite = columns.Text(primary_key = True)
+    errcode = columns.Integer(primary_key = True)
+    diag = columns.Text(primary_key = True)
+    err_count = columns.Counter()
+    job_count = columns.Counter()
+
+    __compaction__ = SizeTieredCompactionStrategy
+    __compaction_max_compaction_threshold__ = 32
+    __compaction_min_compaction_threshold__ = 4
+    __dclocal_read_repair_chance__ = 0.1
+    __default_time_to_live__ = 0
+    __gc_grace_seconds__ = 864000
+    __memtable_flush_period_in_ms__ = 0
+    __read_repair_chance__ = 0.0
